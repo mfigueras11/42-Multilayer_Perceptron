fbe2ac21da3e948930afaa2c417f18d8670df829
Marti Figueras <mfigueras11@gmail.com>
2020-02-17 19:09:15 +0100

    stratified shuffle

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -6,7 +6,7 @@
 #    By: mfiguera <mfiguera@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/14 12:30:53 by mfiguera          #+#    #+#              #
-#    Updated: 2020/02/17 12:29:12 by mfiguera         ###   ########.fr        #
+#    Updated: 2020/02/17 19:08:59 by mfiguera         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,12 +25,27 @@ def categorize(y, categories):
         y[y == name] = id_
     return y
 
+def stratified_shuffle(full, val_size):
+    ones = full[full[:, -1] == 1]
+    zeros = full[full[:, -1] == 0]
+    ratio = len(ones) / len(full)
+    n_ones = int(val_size * ratio // 1)
+    n_zeros = int(val_size * (1-ratio) // 1)
+    while n_ones + n_zeros < val_size:
+        n_zeros += 1
+    train = np.concatenate((ones[:-n_ones], zeros[:-n_zeros]))
+    np.random.shuffle(train)
+    val = np.concatenate((ones[-n_ones:], zeros[-n_zeros:]))
+    np.random.shuffle(val)
+    return train, val
+
 
 data = pd.read_csv("resources/data.csv")
 X = scale(data.to_numpy()[:,2:]).astype(float)
 y = categorize(data["diagnosis"].to_numpy().copy(), ['B', 'M'])
+full = np.concat((X, y))
 X_train, y_train = X[:-50], y[:-50]
 X_val, y_val = X[-50:], y[-50:]
 
 classifier = Model((X.shape[1], 4, 4, 2))
-classifier.train(X_train, y_train, X_val, y_val, n_epoch=200, batch_size=32)
+classifier.train(X_train, y_train, X_val, y_val, n_epoch=25, batch_size=32)
