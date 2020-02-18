1fe829d0a457a25a8559e2513ea21f0abc085376
Marti Figueras <mfigueras11@gmail.com>
2020-02-18 19:37:55 +0100

    new stratified shuffle and better batch_size

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -6,7 +6,7 @@
 #    By: mfiguera <mfiguera@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/14 12:30:53 by mfiguera          #+#    #+#              #
-#    Updated: 2020/02/17 19:08:59 by mfiguera         ###   ########.fr        #
+#    Updated: 2020/02/18 19:37:25 by mfiguera         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,7 +25,7 @@ def categorize(y, categories):
         y[y == name] = id_
     return y
 
-def stratified_shuffle(full, val_size):
+def stratified_shuffle_split(full, val_size):
     ones = full[full[:, -1] == 1]
     zeros = full[full[:, -1] == 0]
     ratio = len(ones) / len(full)
@@ -43,9 +43,10 @@ def stratified_shuffle(full, val_size):
 data = pd.read_csv("resources/data.csv")
 X = scale(data.to_numpy()[:,2:]).astype(float)
 y = categorize(data["diagnosis"].to_numpy().copy(), ['B', 'M'])
-full = np.concat((X, y))
-X_train, y_train = X[:-50], y[:-50]
-X_val, y_val = X[-50:], y[-50:]
+full = np.concatenate((X, y.reshape(y.shape[0], 1)), axis=1)
+train, val = stratified_shuffle_split(full, 75)
+X_train, y_train = train[:, :-1].astype(float), train[:, -1:].reshape((len(train))).astype(float)
+X_val, y_val = val[:, :-1].astype(float), val[:, -1:].reshape((len(val))).astype(float)
 
 classifier = Model((X.shape[1], 4, 4, 2))
-classifier.train(X_train, y_train, X_val, y_val, n_epoch=25, batch_size=32)
+classifier.train(X_train, y_train, X_val, y_val, n_epoch=150, batch_size=1)
