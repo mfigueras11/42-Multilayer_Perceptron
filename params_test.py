96585ad56d8c5a1e4d35e8a378a466aabc846eeb
Marti Figueras <mfigueras11@gmail.com>
2020-02-21 12:30:36 +0100

    added number of hidden layer param

--- params_test.py
+++ params_test.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    multilayer_perceptron.py                           :+:      :+:    :+:    #
+#    params_test.py                                     :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: mfiguera <mfiguera@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/14 12:30:53 by mfiguera          #+#    #+#              #
-#    Updated: 2020/02/21 08:39:57 by mfiguera         ###   ########.fr        #
+#    Updated: 2020/02/21 12:30:30 by mfiguera         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -53,30 +53,31 @@ X_train, y_train = train[:, :-1].astype(float), train[:, -1:].reshape((len(train
 X_val, y_val = val[:, :-1].astype(float), val[:, -1:].reshape((len(val))).astype(float)
 
 
-df = pd.DataFrame(columns=["acc", "n_epoch", "shuffle", "batch_size", "dynamic_lr", "cost_log", "train_log", "val_log"])
-for n_epoch in [10, 100, 250]:
-    for batch_size in [1, 2, 8, 16, 32]:
-        for shuffle in [True, False]:
-            for dynamic_lr in [True, False]:
+df = pd.DataFrame(columns=["acc", "n_epoch", "shuffle", "batch_size", "dynamic_lr", "cost_log", "train_log", "val_log", "hidden_s", "n_hidden", "time"])
+for n_epoch in [100]:
+    for batch_size in [1]:
+        for shuffle in [True]:
+            for dynamic_lr in [True]:
                 for hidden_s in [4, 5, 8]:
-                    classifier = Model((30,hidden_s,hidden_s,2))
-                    t0 = datetime.datetime.now()
-                    cost_log, train_log, val_log = classifier.train(X_train, y_train, X_val, y_val, n_epoch=n_epoch, batch_size=batch_size, dynamic_lr=dynamic_lr, shuffle=shuffle, quiet=True)
-                    t1 = datetime.datetime.now()
-                    print(f"e: {n_epoch} bz: {batch_size} s: {str(shuffle)[0]} d: {str(dynamic_lr)[0]} acc: {classifier.score(classifier.predict(X_val), y_val)}")
-                    filename = classifier.save_to_file(f"test2/e{n_epoch}_bz{batch_size}_s{str(shuffle)[0]}_d{str(dynamic_lr)[0]}_hs{hidden_s}.pickle")
-                    d = {
-                        "acc": classifier.score(classifier.predict(X_val), y_val),
-                        "n_epoch": n_epoch,
-                        "batch_size": batch_size,
-                        "dynamic_lr": dynamic_lr,
-                        "shuffle": shuffle,
-                        "cost_log": cost_log,
-                        "train_log": train_log,
-                        "val_log": val_log,
-                        "hidden_s": hidden_s,
-                        "time": (t1-t0).total_seconds()
-                    }
-                    df = df.append(d, ignore_index=True)
+                    for n_hidden in [1, 2, 3, 4]:
+                        classifier = Model((30,)+(hidden_s,) * n_hidden+(2,))
+                        t0 = datetime.datetime.now()
+                        cost_log, train_log, val_log = classifier.train(X_train, y_train, X_val, y_val, n_epoch=n_epoch, batch_size=batch_size, dynamic_lr=dynamic_lr, shuffle=shuffle, quiet=True)
+                        t1 = datetime.datetime.now()
+                        filename = classifier.save_to_file(f"test3/e{n_epoch}_bz{batch_size}_s{str(shuffle)[0]}_d{str(dynamic_lr)[0]}_hs{hidden_s}.pickle")
+                        d = {
+                            "acc": classifier.score(classifier.predict(X_val), y_val),
+                            "n_epoch": n_epoch,
+                            "batch_size": batch_size,
+                            "dynamic_lr": dynamic_lr,
+                            "shuffle": shuffle,
+                            "cost_log": cost_log,
+                            "train_log": train_log,
+                            "val_log": val_log,
+                            "hidden_s": hidden_s,
+                            "n_hidden":n_hidden,
+                            "time": (t1-t0).total_seconds()
+                        }
+                        df = df.append(d, ignore_index=True)
 
-df.to_csv("experiment.csv")
+df.to_csv("experiment2.csv")
